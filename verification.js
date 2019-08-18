32d2ee724230bfd54ccfa29d9e6c946f7d081b8b
Felix <felix.letheren@gmail.com>
2019-08-18 18:28:32 +0100

    Removes error text when first field is correctly filled.

--- verification.js
+++ verification.js
@@ -3,9 +3,13 @@ var requiredInput = {}
 
 form.addEventListener("submit", function (event) {
         event.preventDefault()
+        document.querySelectorAll('.message-box').innerHTML = ''
         requiredInput['required'] = form.requiredField.value
         if (requiredInput['required'] === '') {
             document.getElementById('requiredFieldMessageBox').innerHTML = '<p class="error-message">This field is required. Please enter some text</p>'
+        }
+        else{
+            document.getElementById('requiredFieldMessageBox').innerHTML = ''
         }
     }
 )
