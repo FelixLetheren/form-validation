0a9fa7b85619dfcc0abc67a57fdd1ec7c2c1e4ea
Felix <felix.letheren@gmail.com>
2019-08-18 18:50:03 +0100

    Gave names and id's to HTML fields

--- verification.js
+++ verification.js
@@ -3,13 +3,12 @@ var requiredInput = {}
 
 form.addEventListener("submit", function (event) {
         event.preventDefault()
-        document.querySelectorAll('.message-box').innerHTML = ''
         requiredInput['required'] = form.requiredField.value
         if (requiredInput['required'] === '') {
-            document.getElementById('requiredFieldMessageBox').innerHTML = '<p class="error-message">This field is required. Please enter some text</p>'
+            document.getElementById('required-field-message-box').innerHTML = '<p class="error-message">This field is required. Please enter some text</p>'
         }
         else{
-            document.getElementById('requiredFieldMessageBox').innerHTML = ''
+            document.getElementById('required-field-message-box').innerHTML = ''
         }
     }
 )
